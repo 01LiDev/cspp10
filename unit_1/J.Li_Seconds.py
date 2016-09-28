4ff43272cb78ccaeb47bb9f265289b2174cf431d
WIndx15 <johnsonli@uagateway.org>
2016-09-27 21:37:02 -0400

    Update J.Li_Seconds.py

--- unit_1/J.Li_Seconds.py
+++ unit_1/J.Li_Seconds.py
@@ -1,6 +1,10 @@
+#seconds= remander of the seconds input -_-
+#minute=60 seconds
+#hour=3600 seconds
 s= int(input())
-m=s/60 /60
+m=s/60 
 seconds=s%60
 hour= m/60
+hour=s/3600
 print( str(int(hour)) + " hour " + str(int(m)) + " minute " + str(int(seconds)) + " seconds ")
 
