f28718de3303dd7016a3df8f12c469f3cf983691
Johnson LI <johnsonli@uagateway.org>
2016-10-26 16:15:54 +0000

    Fizz Buzz Generator

---
+++ Class With Chen ._./input.py
@@ -0,0 +1,8 @@
+final=""
+inp = input("enter a number or exit:")
+while (inp != "exit"):
+    final = final+inp+" "
+    inp = input("enter a number or exit:")
+    
+    
+print(final)
---
+++ unit 3/FizzBuzz_generator.py
@@ -0,0 +1,5 @@
+Fizz = int(input("First Divisible: "))
+Buzz = int(input("Second Divisible: "))
+n= Fizz or Buzz
+if ((n % fizz) == 0):
+    print() 
