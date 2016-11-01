8bb52816f47af1839618c2a539cebb7002bddce3
Johnson LI <johnsonli@uagateway.org>
2016-11-01 16:16:02 +0000

    Num Com

---
+++ unit 3/JL_numguess.py
@@ -0,0 +1,20 @@
+import random
+user = int(input("Guess the number: "))
+correct=random.randint(1,100)
+guesses= 0
+while user != correct:
+    guesses = guesses + 1
+    print (user)
+    if user < correct:
+        print("Too low")
+        user = int(input("Guess the number: "))
+    elif user > correct:
+        print("Too high")
+        user = int(input("Guess the number: "))
+    else:
+        print (user)
+print ("You are correct. It took you {} tries.".format(guesses))
+    
+        
+    
+    
