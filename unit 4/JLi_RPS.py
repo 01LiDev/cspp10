7fa7f06c733955c7078e95e0fe37c1647974abd7
Johnson LI <johnsonli@uagateway.org>
2016-11-29 17:17:39 +0000

    RPS SO DAM CLOSEgit add .git add .

--- unit 4/JLi_RPS.py
+++ unit 4/JLi_RPS.py
@@ -6,54 +6,65 @@ def get_comp_move():
     cmove= random.choice( ['Rock', 'Paper', 'Scissors'] )
     return cmove
 def get_rounds():
-    rounds = int(input("Rounds: "))
-    return rounds
+    user_rounds = int(input("Rounds: "))
+    return user_rounds
 def get_round_winner(p1move, cmove):
     if p1move == "Rock" and cmove == "Scissors":
-        return p1move
+        return "player"
     elif p1move == "Scissors" and cmove == "Paper":
-        return p1move
+        return "player"
     elif p1move == "Paper" and cmove == "Rock":
-        return p1move
+        return "player"
     elif cmove == "Rock" and p1move =="Scissors":
-        return cmove
+        return "comp"
     elif cmove == "Paper" and p1move =="Rock":
-        return cmove
+        return  "comp"
     elif cmove == "Scissors" and p1move =="Paper":
-        return cmove
-    elif p1move == cmove:
-        return ties
-def get_full_move(shortmove):
-    if p1move == "R" or "r" or "rock":
+        return  "comp"
+    elif cmove == p1move:
+        return "tie"
+def get_full_move(p1move):
+    if p1move == "R" or p1move == "r" or  p1move == "rock":
         return "Rock"
-    if p1move == "P" or "p" or "paper":
+    elif p1move == "P" or p1move == "p" or p1move == "paper":
         return "Paper"        
-    if p1move == "S" or "s" or "scissors":
+    elif p1move == "S" or p1move == "s" or p1move == "scissors":
         return "Scissors"    
+    else:
+        return "none"
 def print_score(pscore, cscore, ties):
+    print ("'''''''''")
     print("Scoreboard")
     print("----------")
     print("Player: {}".format(pscore))
     print("Computer: {}".format(cscore))
     print("Ties: {}".format(ties))
-#function name: rps
-#   arguments: none
-#   purpose: the main game loop.  This should be the longest, using
-#               all the other functions to create RPS
-#   returns: none
 def rps():
-    print (get_rounds())
-    print (get_p1_move())
-    print (get_comp_move())
-    print (print_score(p1move,cmove,ties))
-
-#function name: tests
-#   arguments: none
-#   purpose: a place for you to write your tests.  replace 'rps' below
-#               with 'tests' to run this function instead of the game loop
-#   returns: none
-def test():
-    return 1
-    #code here
-
+    pscore = 0
+    cscore = 0
+    ties = 0
+    for x  in range(get_rounds()):
+        p1move = get_p1_move()
+        cmove = get_comp_move()
+        winner = get_round_winner(p1move,cmove)
+        print ("Player chose {}".format(p1move))
+        print ("Computer chose {}".format(cmove))
+        if winner == "player":
+            print ("Player win")
+            pscore = pscore +1
+            print_score(pscore,cscore,ties)
+        elif winner == "comp":
+            print("Computer win")
+            cscore = cscore +1
+            print_score(pscore,cscore,ties)
+        elif winner == "tie":
+            print("Its a tie")
+            ties = ties + 1
+            print_score(pscore,cscore,ties)
+    if pscore > cscore:
+        print ("Player won the game!")
+    elif cscore > pscore:
+        print ("Computer won the game!")
+    else:
+        print("Game is tied")
 rps()
