e5e9c393ed705592de70ec5817be615e66bbd714
Johnson LI <johnsonli@uagateway.org>
2016-12-05 17:16:20 +0000

    finished RPS

--- Class With Chen ._./input.py
+++ Class With Chen ._./input.py
@@ -1,8 +1,15 @@
-final=""
-inp = input("enter a number or exit:")
-while (inp != "exit"):
-    final = final+inp+" "
-    inp = input("enter a number or exit:")
+# final=""
+# inp = input("enter a number or exit:")
+# while (inp != "exit"):
+#     final = final+inp+" "
+#     inp = input("enter a number or exit:")
     
     
-print(final)
+# print(final)
+
+from getpass import getpass
+
+asdf = input("This is not secure: ")
+pw = getpass("This is secure: ")
+
+print("You typed: " + pw)
--- unit 4/JLi_RPS.py
+++ unit 4/JLi_RPS.py
@@ -1,13 +1,23 @@
 import random
 def get_p1_move():
-    p1move = input("Rock;Paper;Scissors: ")
+    p1move = input("User Choose-Rock;Paper;Scissors: ")
+    while p1move != "Rock" and p1move != "Paper" and p1move != "Scissors":
+        p1move = input("Invalid, type: Rock;Paper;Scissors: ")
     return p1move
+
 def get_comp_move():
     cmove= random.choice( ['Rock', 'Paper', 'Scissors'] )
     return cmove
+
 def get_rounds():
-    user_rounds = int(input("Rounds: "))
+    user_rounds = -1
+    while user_rounds > 9 or user_rounds < 1:
+        try:
+            user_rounds = int(input("How many Rounds?[1:9]: "))
+        except:
+            print("invalid input, try again")
     return user_rounds
+
 def get_round_winner(p1move, cmove):
     if p1move == "Rock" and cmove == "Scissors":
         return "player"
@@ -40,10 +50,11 @@ def print_score(pscore, cscore, ties):
     print("Computer: {}".format(cscore))
     print("Ties: {}".format(ties))
 def rps():
+    rounds = get_rounds()
     pscore = 0
     cscore = 0
     ties = 0
-    for x  in range(get_rounds()):
+    for x  in range(rounds):
         p1move = get_p1_move()
         cmove = get_comp_move()
         winner = get_round_winner(p1move,cmove)
@@ -61,10 +72,15 @@ def rps():
             print("Its a tie")
             ties = ties + 1
             print_score(pscore,cscore,ties)
+    
     if pscore > cscore:
+        print ("In {} rounds".format(rounds))
         print ("Player won the game!")
+        
     elif cscore > pscore:
+        print ("In {} rounds".format(rounds))
         print ("Computer won the game!")
     else:
+        print ("In {} rounds".format(rounds))
         print("Game is tied")
 rps()
