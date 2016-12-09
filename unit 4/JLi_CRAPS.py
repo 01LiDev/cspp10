2c4b6f622c3990238d76cbbeb50a17227fca832e
Johnson LI <johnsonli@uagateway.org>
2016-12-09 17:15:51 +0000

    Made the craps process better

--- unit 4/JLi_CRAPS.py
+++ unit 4/JLi_CRAPS.py
@@ -4,57 +4,64 @@ def piggy_bank():
     return piggy_bank
 def player_bet():
     print ("Bank Amount: {}".format (piggy_bank()))
-    user_bet=int(input("Enter a whole number or type hold:"))
+    user_bet=input("Enter a whole number or type hold:")
     return user_bet
 def validator():
-    the_bet = player_bet()
-    while the_bet < 1 and the_bet > 100:
-        if the_bet < 1:
+    while player_bet() < 1 or player_bet() > 100:
+        if player_bet() < 1:
             print("Invalid amount")
-        elif the_bet > piggy_bank():
+        elif player_bet() > piggy_bank():
             print("You don't have enough ")
-        elif the_bet != int():
+        elif player_bet() != int():
             print("Not a whole number")
-        elif the_bet == "hold":
-            return the_bet
-        else:
-            return the_bet
-def house():
+        elif player_bet() == "hold":
+            return "holded"
+        
+    return "valid"
+def dices():
     dice_1 = random.randint(1,6)
     dice_2 = random.randint(1,6)
     sum_dice = dice_1 + dice_2
-    print ("Rolled: {} and {}".format(dice_1,dice_2))
-    print ("First roll:{}".format(sum_dice))
     return sum_dice
 def first_roll():
-    roll_1 = house()
-    if roll_1 == 2 or roll_1 == 3 or roll_1 == 12:
-        print ("You lost")
+    if dices() == 2 or dices() == 3 or dices() == 12:
         return "lost"
-    if roll_1 == 7 or roll_1 == 11:
-        print ("You win")
+    if dices() == 7 or dices() == 11:
         return "win"
         
 def point_roll():
-    point_roll = house()
-    re_roll = house()
-    if re_roll == 7 :
-        print ("End round,You lost.")
+    if dices() == 7 :
         return "lost"
-    elif re_roll == point_roll:
-        print ("You win")
+    elif dices() == dices():
         return "win"
 def craps():
-    user_turn = player_bet()
-    house_turn = house()
+    total_win = 0
+    house_turn = dices()
     detector = validator()
     bank = piggy_bank()
-    if first_roll() == "win":
-        bank = bank + player_bet()
-    elif first_roll() == "lost":
-        bank = bank - player_bet()
-    elif  user_turn == "hold":
-        print ("player holded bank amount {}$".format(bank))
+    point_dice=point_roll()
+    while detector == "valid":
+        print ("You bet {}".format(player_bet()))
+        if first_roll() == "win":
+            total_win = player_bet() + player_bet()
+            bank = bank +total_win
+            print ("You won")
+            print ("You now have {}$".format(bank))
+        elif point_dice == "win":
+            total_win = player_bet() + player_bet()
+            bank = bank + total_win
+            print ("You won")
+            print ("You now have {}$".format(bank))
+        elif first_roll() == "lost":
+            bank = bank - player_bet()
+            print ("You lost to house")
+            print ("You now have {}$".format(bank))
+        elif point_dice == "lost":
+            bank = bank - player_bet()
+            print ("You lost to house")
+            print ("You now have {}$".format(bank))
+        elif validator() == "holded":
+            print ("player holded bank amount {}$".format(bank))
         
            
     
