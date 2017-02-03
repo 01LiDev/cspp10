e1e86c259d4f48b776240052fc3a8bffa441b7fc
Johnson LI <johnsonli@uagateway.org>
2017-02-03 17:15:47 +0000

    Doing scrmbing phrase

--- unit 5/JLi_scramble.py
+++ unit 5/JLi_scramble.py
@@ -2,19 +2,25 @@ import random as r
 #function:scramble_word
 #purpose:scramble a word but not the last or first letters
 #arguments:the word
-def scramble_word(word):
-    if len(word) > 3:
-        list_w = list(word)
+def scramble_word(words):
+    if  len(words) > 3:
+        list_w = list(words)
         fl = list_w[0]
         ll = list_w[-1]
         w = list_w[1:-1]
         r.shuffle(w)
         product = "".join(w)
         result = fl + product + ll
-        print (result)
         return result
-# def scramble_phrase(a,b,c):
-#      ts = 
+def scramble_phrase(s):
+     words = s.split(" ")
+     scramble_word(words)
+     print (words)
+     
+         
+         
+      
+
+      
     
-# scramble_phrase("laughing","over","summer")
-scramble_word("cantolope")
+scramble_phrase("laughing over summer")
