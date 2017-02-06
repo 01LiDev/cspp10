843506663bfc2373d4e74fecfe40b41f04462a2d
Johnson LI <johnsonli@uagateway.org>
2017-02-06 17:16:19 +0000

    working on phrase

--- unit 5/JLi_scramble.py
+++ unit 5/JLi_scramble.py
@@ -2,20 +2,28 @@ import random as r
 #function:scramble_word
 #purpose:scramble a word but not the last or first letters
 #arguments:the word
-def scramble_word(words):
-    if  len(words) > 3:
-        list_w = list(words)
+def scramble_word(result):
+    if  len(result) > 3:
+        list_w = list(result)
         fl = list_w[0]
         ll = list_w[-1]
         w = list_w[1:-1]
-        r.shuffle(w)
+        r.shuffle(result)
         product = "".join(w)
         result = fl + product + ll
         return result
 def scramble_phrase(s):
      words = s.split(" ")
-     scramble_word(words)
-     print (words)
+     fw = words[0]
+     sw = words[1]
+     lw = words[2]
+     
+     scramble_word(fw)
+     scramble_word(sw)
+     scramble_word(lw)
+     total = fw + sw + lw
+     overall = "".join(total)
+     print (overall)
      
          
          
