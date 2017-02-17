034f09a6819da3431789ee56fd60e871f341b824
Johnson LI <johnsonli@uagateway.org>
2017-02-17 17:15:39 +0000

    Done

--- dic/dic.py
+++ dic/dic.py
@@ -1,41 +1,64 @@
 from pprint import pprint
 dict_assg = {
-    'hay':'bay'
+
 }
 #This should allow the user to add a new key-value pair to the dictionary.
 #Ask the user for both the key and the value.  Treat both as Strings.\
 def add(dict_assg):
     key_input = input("Enter a key: ")
-    value_input = input("Enter a value")
+    value_input = input("Enter a value: ")
     print("New key-value pair {},{}".format(key_input,value_input))
-    dict_assg[key_input] = value_input
-    
+    if key_input not in dict_assg:
+        dict_assg[key_input] = value_input
+    else:
+        print("Key already made")
+        key_input
+        value_input
 #This should allow the user to remove a key-value pair from the dictionary.
 #Ask the user for just the key and delete based on the key.
-def remove_key():
+def remove_key(dict_assg):
     keyval_rem = input("The key you want to remove: ")
-    
-#This should allow the user to remove multiple key-value pairs from the 
-#dictionary based on a value.  Ask the user for just a value and remove all keys 
-#from the dictionary that have that value.
-# def remove_value(extension):
+    if keyval_rem in dict_assg:
+        del [keyval_rem]
 
 #This should allow the user to change a key/value pair in the dictionary.
-# def update():
+def update(dict_assg):
+    print("Key to be updated")
+    update_user = input("Enter: ")
+    if update_user in dict_assg:
+        print("New Value")
+        upd_val = input("Enter:")
+        dict_assg[update_user] = upd_val
 
 #This should prettyprint (pprint) the dictionary.
-# def p_print():
-
+def p_print(dict_assg):
+    pprint(dict_assg)
 #This should run the print function and then end the program.
-# def exit():
+def exit():
+    p_print(dict_assg)
 
 #The main function
 def main():
-    while True:
-        options = input("")
-    
-add(dict_assg)
-pprint(dict_assg)
+    print("Press enter to continue or Exit to stop")
+    options = input("Enter: ").lower()
+    while options != "exit":
+        print("Your options are Add|Remove|Update|Pprint|Exit")
+        options = input("Enter: ").lower()
+        if options == "add":
+            add(dict_assg)
+        elif options == "remove":
+            remove_key(dict_assg)
+        elif options == "update":
+            update(dict_assg)
+        elif options == "pprint":
+            p_print(dict_assg)
+        elif options == "exit":
+            exit()
+            break
+        
+        
+main()
+
 
 
 
